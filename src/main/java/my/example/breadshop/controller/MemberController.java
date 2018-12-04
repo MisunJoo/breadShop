b15ee58b5993f3226639943b4b0eb6c4bb72400f
MisunJoo <0987someday@naver.com>
2018-12-04 21:32:18 +0900

    Add: MemberController signup

--- src/main/java/my/example/breadshop/controller/MemberController.java
+++ src/main/java/my/example/breadshop/controller/MemberController.java
@@ -1,6 +1,7 @@
 package my.example.breadshop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,4 +15,14 @@ public class MemberController {
         return "login";
     }
 
+    @GetMapping("/member/signup")
+    public String signUpForm(ModelMap modelMap, @RequestParam(value = "email", defaultValue = "") String email,
+                             @RequestParam(value = "nickName", defaultValue = "")String nickName,
+                             @RequestParam(value = "duplication", defaultValue = "") String duplication) {
+        modelMap.addAttribute("email",email);
+        modelMap.addAttribute("nickName",nickName);
+        modelMap.addAttribute("duplication",duplication);
+
+        return "signup";
+    }
 }
